247109666cc18896dd2903390a96c97742e4ee41
Jay Millena <jay.millena07@gmail.com>
2024-01-07 15:23:47 +0800

    Literals on displaying binary

--- Hello.java
+++ Hello.java
@@ -2,17 +2,9 @@ public class Hello
 {
   public static void main(String[] arguments)
   {
-    int regularNumber = 2;
-    byte inch = 127;
-    short centimeter = 558;
-    long length = 58554l;
-    float oldWeight = 60.8f;
-    double currentWeight = 56.8f;
-
-    char rankClass = 'c';
-    boolean isElite = false;
-
-    System.out.println();
+    // Literals
+    int number = 0b101;
+    System.out.println(number);
   }   
 }
 
