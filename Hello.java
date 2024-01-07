9d16f9de66737f6e7b2256b654e97ac1b710c596
Jay Millena <jay.millena07@gmail.com>
2024-01-07 15:37:42 +0800

    Equation without type casting

--- Hello.java
+++ Hello.java
@@ -2,9 +2,11 @@ public class Hello
 {
   public static void main(String[] arguments)
   {
-    // Literals, character
-    char blood_type = 'a'; 
-    System.out.println(blood_type);
+    // Literals, without type casting on type conversion
+    byte b = 127;
+    int a = 12;
+
+    System.out.println(a == b);
   }   
 }
 
