0748a0cb991165208b2d9e4795a7b221325c31b7
Jay Millena <jay.millena07@gmail.com>
2024-01-07 15:18:59 +0800

    Primitive datatypes

--- Hello.java
+++ Hello.java
@@ -2,10 +2,17 @@ public class Hello
 {
   public static void main(String[] arguments)
   {
-    int firstNumber = 2;
-    int secondNumber = 5;
-    int result = firstNumber + secondNumber;
-    System.out.println(result);
+    int regularNumber = 2;
+    byte inch = 127;
+    short centimeter = 558;
+    long length = 58554l;
+    float oldWeight = 60.8f;
+    double currentWeight = 56.8f;
+
+    char rankClass = 'c';
+    boolean isElite = false;
+
+    System.out.println();
   }   
 }
 
