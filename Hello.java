17db67ed02e8e2b90b496d1af50663104a63b904
Jay Millena <jay.millena07@gmail.com>
2024-01-07 16:05:40 +0800

    Relational operators in Java

--- Hello.java
+++ Hello.java
@@ -3,13 +3,14 @@ public class Hello
   public static void main(String[] arguments)
   {
     // Define the variable number
-    int number = 6;
+    int number1 = 6;
+    int number2 = 6;
 
-    // Incrementing number using pre increment
-    number--;
+    // Check the equality relationship of 2 numbers
+    boolean result = number1 == number2;
 
     // Displaying the result
-    System.out.println(number);
+    System.out.println(result);
   }   
 }
 
