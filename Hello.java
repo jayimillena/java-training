d057df0cdc5fb416cf298b41451230affc83a4e6
Jay Millena <jay.millena07@gmail.com>
2024-01-07 15:40:33 +0800

    With type casting

--- Hello.java
+++ Hello.java
@@ -2,11 +2,11 @@ public class Hello
 {
   public static void main(String[] arguments)
   {
-    // Literals, without type casting on type conversion
+    // Literals, with type casting on type conversion
     byte b = 127;
     int a = 12;
 
-    System.out.println(a == b);
+    System.out.println(b == (byte) a);
   }   
 }
 
